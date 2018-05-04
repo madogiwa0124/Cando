84a8e92672a9d7af54efd027b3bc52e2582f94b7
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-05-04 11:08:06 +0900

    tasksにtitleとstatusの複合インデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_28_090605) do
+ActiveRecord::Schema.define(version: 2018_05_04_014111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_04_28_090605) do
     t.datetime "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title", "status"], name: "index_tasks_on_title_and_status"
   end
 
 end
