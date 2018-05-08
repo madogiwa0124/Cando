64897a730b47b377aaaa300adad0c3021a49ae32
Morita Jun <morita.jn0124@gmail.com>
2018-05-08 23:33:41 +0900

    rootをタスク一覧に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
   resources :tasks do
     collection{ get :search }
   end
