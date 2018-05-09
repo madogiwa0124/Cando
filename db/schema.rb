1ad5a396a7c3effc4cd75135ba4eac8908d623f3
Morita Jun <morita.jn0124@gmail.com>
2018-05-10 00:14:42 +0900

    rails g sorcery:installを実行 migrationについては下記を参考に手動で作成 https://github.com/Sorcery/sorcery/issues/119

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_014111) do
+ActiveRecord::Schema.define(version: 2018_05_09_151024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2018_05_04_014111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title", "status"], name: "index_tasks_on_title_and_status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
