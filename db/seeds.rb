4611392c21cae84fce51c241ea009ce47d6512df
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-05-12 22:41:22 +0900

    seedデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 # タスクの作成
-100.times do |n|
-  FactoryBot.create(:task, title: "タイトル_#{n}")
-end
-
 5.times do |n|
   FactoryBot.create(:user, name: "test_user_#{n}")
+end
+
+100.times do |n|
+  case n % 4
+  when 0 then FactoryBot.create(:task, :todo, :low, title: "タイトル_#{n}", user: User.first)
+  when 1 then FactoryBot.create(:task, :doing, :medium, title: "タイトル_#{n}", user: User.second)
+  when 2 then FactoryBot.create(:task, :done, :high, title: "タイトル_#{n}", user: User.third)
+  when 3 then FactoryBot.create(:task)
+  end
 end
