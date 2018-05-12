e93c2f9e0d62129b2303092e89cecde1c2ab1f93
Morita Jun <morita.jn0124@gmail.com>
2018-05-12 15:29:45 +0900

    ユーザーとタスクのassociationを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :tasks 
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, length: { maximum: 255 }, uniqueness: true, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :name, length: { maximum:255 }, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_151024) do
+ActiveRecord::Schema.define(version: 2018_05_12_062121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2018_05_09_151024) do
     t.datetime "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["title", "status"], name: "index_tasks_on_title_and_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
