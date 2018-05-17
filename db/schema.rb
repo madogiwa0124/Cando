4d97e0d746455d8b338a7f1f9f36de44ecfb867e
Morita Jun <morita.jn0124@gmail.com>
2018-05-18 00:01:17 +0900

    roleテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_062121) do
+ActiveRecord::Schema.define(version: 2018_05_17_145559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "code", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
