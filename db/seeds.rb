8cebb22d889d9a263bfeeacb8d787fc3c37abaa6
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-05-18 23:37:30 +0900

    roleのデフォルト値を一般とするようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@
   FactoryBot.create(:user, name: "test_user_#{n}")
 end
 
+1.times do |n|
+  FactoryBot.create(:user, name: "admin_user", email: 'administrater@email.com')
+end
+
 100.times do |n|
   case n % 4
   when 0 then FactoryBot.create(:task, :todo, :low, title: "タイトル_#{n}", user: User.first)
