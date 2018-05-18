f261038d2cb5b351901e5443d39ce8f627121364
Morita Jun <morita.jn0124@gmail.com>
2018-05-19 00:49:37 +0900

    ユーザー関連のメッセージのi18n対応

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -24,7 +24,7 @@ module Admin
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to admin_user_path(@user), notice: 'User was successfully created.'
+        redirect_to admin_user_path(@user), notice: message('user', 'create')
       else
         render :new
       end
@@ -33,7 +33,7 @@ module Admin
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-        redirect_to admin_user_path(@user), notice: 'User was successfully updated.'
+        redirect_to admin_user_path(@user), notice: message('user', 'update')
       else
         render :edit
       end
@@ -42,7 +42,7 @@ module Admin
     def destroy
       @user = User.find(params[:id])
       @user.destroy
-      redirect_to admin_users_url, notice: 'User was successfully destroyed.'
+      redirect_to admin_users_url, notice: message('user', 'destroy')
     end
 
     private
