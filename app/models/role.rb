efa31faa248165c0d36a3aa08bdf681f16a53876
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-05-19 00:36:15 +0900

    :cop:

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,8 @@
 class Role < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-    {id: 1, code: :admin, name: I18n.t('enums.role.code.admin') },
-    {id: 2, code: :staff, name: I18n.t('enums.role.code.staff') }
+    { id: 1, code: :admin, name: I18n.t('enums.role.code.admin') },
+    { id: 2, code: :staff, name: I18n.t('enums.role.code.staff') }
   ]
 
   def admin?
