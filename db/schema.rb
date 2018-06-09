0d30e8c48f39db85f1586007628d36c1900f00ce
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-06-09 15:25:44 +0900

    Groupモデルの名前は必須に変更

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2018_06_09_055831) do
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
